f969661684aa90478ba546ac0b5e92cf77d88bfa
Heisey <jheisler01@gmail.com>
2020-11-04 13:54:10 -0800

    started posts server

---
+++ posts/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const chalk = require('chalk')
+const { v4: uuid } = require('uuid')
+
+const Posts = {}
+
+const app = express()
+
+const PORT = process.env.PORT || 4500
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+  res.status(200).json({
+    "status": "success",
+    "records": Posts
+  })
+})
+
+app.post('/posts', (req, res) => {
+  const id = uuid()
+  console.log(req.body)
+
+  const { title } = req.body
+
+  const post = {
+    id,
+    title
+  }
+
+  Posts[id] = post
+
+  res.status(201).json({
+    "status": "success",
+    "records": post
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(chalk.bold.blue.inverse(`Post micorservice is running on ${PORT}`))
+})
